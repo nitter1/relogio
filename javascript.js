1068040ec5e2e8bd8853e39524b7464ad6dce3c2
Daniel Roger Nitter <100990223+nitter1@users.noreply.github.com>
2022-10-08 00:58:08 -0300

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -10,29 +10,29 @@ var segundos = data.getSeconds()
 msg.innerHTML = `Agora são ${hora} horas e ${minutos} minutos`
 if (hora >= 6 && hora <= 7) {
     // Bom Dia das 06h às 7h
-    img.src = 'img/fotoamanhecer.png'
+    img.src = '/img/fotoamanhecer.png'
     document.body.style.background = '#EB8F08'
 } else if (hora > 7 && hora < 12) {
     // Bom Dia das 7h às 12h
-    img.src = 'img/fotomanha.png'
+    img.src = '/img/fotomanha.png'
     document.body.style.background = '#F9F08B'
 } else if (hora >= 12 && hora <= 16) {
     //Boa Tarde das 12h às 15h
-    img.src = 'img/fototarde.png'
+    img.src = '/img/fototarde.png'
     document.body.style.background = '#FFD700'
 } else if (hora > 16 && hora < 18) {
     //Boa Tarde das 15h às 17h
-    img.src = 'img/fotofimdetarde.png'
+    img.src = '/img/fotofimdetarde.png'
     document.body.style.background = '#9D444C'
 } else if (hora >= 18 && hora < 20) {
     //Boa Noite das 20 às 00
-    img.src = 'img/fotonoite.png'
+    img.src = '/img/fotonoite.png'
     document.body.style.background = '#1f2130'
 } else if (hora >= 20 && hora <= 23) {
-    img.src = 'img/fotonoite2.png'
+    img.src = '/img/fotonoite2.png'
     document.body.style.background = '#0e1017'
 } else if (hora >= 0 && hora < 6) {
-    img.src = '../Site Hora/img/fotonoite3.png'
+    img.src = '/img/fotonoite3.png'
     document.body.style.background = 'black'
 }
 
